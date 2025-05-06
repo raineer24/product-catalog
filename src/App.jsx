c5a0856798dd11acc2bfda996d98750ca5c8ff7e
raineer <raineerdelarita@gmail.com>
2025-05-07 00:41:28 +0800

    app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,5 +4,16 @@ import ProductPage from './pages/ProductPage';
 import './App.css';
 
 function App() {
-  return ()
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/product/:id' element={<ProductPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
+
+export default App;
